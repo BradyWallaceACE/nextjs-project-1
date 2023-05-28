f126208f5e953c44837233dfd82a0ada2fb657ca
BradyWallaceACE <bwallace2@aceproject.space>
2023-05-28 16:41:37 -0500

    Dynamic page generation for dynamic paths
    It's wild

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,18 +1,15 @@
-import { useRouter } from "next/router";
 import { Fragment } from "react";
 
-import { getEventById } from "../../dummy-data";
+import { getEventById } from "../../helpers/api-utils";
+import { getAllEvents } from "../../helpers/api-utils";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
 import ErrorAlert from "../../components/ui/error-alert";
 import Button from "../../components/ui/button";
 
-function EventDetailPage() {
-  const router = useRouter();
-
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+function EventDetailPage(props) {
+  const event = props.selectedEvent;
 
   if (!event) {
     return (
@@ -41,6 +38,29 @@ function EventDetailPage() {
       </EventContent>
     </Fragment>
   );
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+
+  const paths = events.map((event) => ({ params: { eventId: event.id } }));
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
 }
 
 export default EventDetailPage;
